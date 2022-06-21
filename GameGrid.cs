aa83acc934e88ec4d9a2d6e59f0daad270e44e3a
kachanov mykola <76394135+Klaimen921@users.noreply.github.com>
2022-06-21 10:17:53 +0300

    Add files via upload

---
+++ GameGrid.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+using Tetris.Utilities;
+
+namespace Tetris
+{
+    class GameGrid
+    {
+        public bool IsPlaying { get; set; }
+        public bool GameEnded { get; private set; }
+        public int GameSpeed { get; private set; } = 1;
+        private double position;
+        public int BurnedLines { get; private set; }
+        public int Scores { get; private set; }
+        public int[][] Grid { get; }
+        public int Rows { get; }
+        public int Columns { get; }
+        public Figure ActiveFigure { get; private set; }
+        public Figure NextFigure { get; private set; }
+
+        #pragma warning disable CS8618
+        public GameGrid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            Grid = new int[Rows][];
+            for (int i = 0; i < Grid.Length; i++)
+            {
+                Grid[i] = new int[Columns];
+            }
+            StartNewGame();
+        }
+
+
+        // оновлення
+        public void Update(TimeSpan deltaTime)
+        {
+            position += deltaTime.TotalSeconds * GameSpeed * 3;
+            int moveDownOn = (int)Math.Truncate(position);
+            position -= moveDownOn;
+            for (int i = 0; i < moveDownOn; i++)
+            {
+                if (ActiveFigure.MoveDown(Grid))
+                {
+                    GetNextFigure();
+                    return;
+                }
+            }
+        }
+        private void GetNextFigure()
+        {
+            ActiveFigure.EndMove(Grid);
+            BurnLinesCheck();
+
+            if (!LineUtility.IsEmpty(0, Grid)) // перевырка програшу
+            {
+                GameEnded = true;
+                return;
+            }
+
+            // Прискорювати
+            GameSpeed = (BurnedLines / 20) + 1;
+
+            // Отримати далі
+            ActiveFigure = NextFigure;
+            NextFigure = FigureUtility.GenerateRandomFigure();
+        }
+        private void BurnLinesCheck()
+        {
+            // перевірити та видалити рядки
+            var lines = Enumerable.Range(0, Rows);
+
+            int lines_count = 0;
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                foreach (int line in lines)
+                {
+                    if (LineUtility.IsFull(line, Grid))
+                    {
+                        LineUtility.DeleteLine(line, Grid);
+                        lines_count++;
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+
+            // підрахунок балів
+            BurnedLines += lines_count;
+            if (lines_count == 1)
+            {
+                Scores += 100;
+            }
+            else if (lines_count == 2)
+            {
+                Scores += 300;
+            }
+            else if (lines_count == 3)
+            {
+                Scores += 700;
+            }
+            else if (lines_count == 4)
+            {
+                Scores += 1500;
+            }
+        }
+
+
+        // контроль
+        public void TryMoveFigureRight() => ActiveFigure?.MoveRight(Grid);
+        public void TryMoveFigureLeft() => ActiveFigure?.MoveLeft(Grid);
+        public void TryMoveFigureDownFast()
+        {
+            while (true)
+            {
+                if (ActiveFigure.MoveDown(Grid))
+                {
+                    GetNextFigure();
+                    return;
+                }
+            }
+        }
+        public void TryRotateFigure() => ActiveFigure?.Rotate(Grid);
+        public void StartNewGame()
+        {
+            for (int i = 0; i < Grid.Length; i++)
+            {
+                for (int j = 0; j < Grid[0].Length; j++)
+                {
+                    Grid[i][j] = 0;
+                }
+            }
+            ActiveFigure = FigureUtility.GenerateRandomFigure();
+            NextFigure = FigureUtility.GenerateRandomFigure();
+
+            GameEnded = false;
+            IsPlaying = true;
+
+            BurnedLines = 0;
+            GameSpeed = 1;
+            position = 0;
+            Scores = 0;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Tetris.Items;
+
+namespace Tetris
+{
+    public partial class MainWindow: Window
+    {
+        private const int CELL_SIZE = 25;
+        private readonly int framesPerSecond = 50;
+        private TimeSpan CalculateDelay => TimeSpan.FromMilliseconds(1000.0 / framesPerSecond);
+
+        GameGrid gameGrid;
+
+        private readonly Image[,] imageControls;
+        private readonly ImageSource[] tileImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/gg.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileRed.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TilePurple.png", UriKind.Relative)),
+        };
+        private readonly ImageSource[] button_images = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/icon_play.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative))
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gameGrid = new GameGrid(rows: 20, columns: 10);
+            imageControls = InitGameCanvas();
+            StartGameLoop();
+        }
+        private Image[,] InitGameCanvas()
+        {
+            int[][] matrix = gameGrid.Grid;
+            Image[,] imageControls = new Image[matrix.Length, matrix[0].Length];
+
+            for (int r = 0; r < matrix.Length; r++)
+            {
+                for (int c = 0; c < matrix[0].Length; c++)
+                {
+                    Image imageControl = new()
+                    {
+                        Width = CELL_SIZE,
+                        Height = CELL_SIZE
+                    };
+                    Canvas.SetTop(imageControl, ((r - 2) * CELL_SIZE) + 50);
+                    Canvas.SetLeft(imageControl, (c * CELL_SIZE) + 0);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                    imageControls[r, c].Opacity = 1;
+                    imageControls[r, c].Source = tileImages[0];
+                }
+            }
+
+            return imageControls;
+        }
+        private void DrawNextFigureCanvas()
+        {
+            nextShapeCanvas.Children.Clear();
+
+            Figure? next_figure = gameGrid.NextFigure;
+            if (next_figure is null) return;
+            Square[] container = next_figure.container;
+
+            foreach (Square item in container)
+            {
+                int r = item.x;
+                int c = item.y;
+
+                Image imageControl = new()
+                {
+                    Width = CELL_SIZE,
+                    Height = CELL_SIZE
+                };
+                Canvas.SetTop(imageControl, ((r - 2) * CELL_SIZE) + 50);
+                Canvas.SetLeft(imageControl, (c * CELL_SIZE) + 0);
+                nextShapeCanvas.Children.Add(imageControl);
+                imageControl.Source = tileImages[item.Type];
+            }
+        }
+
+        private async void StartGameLoop()
+        {
+            while (true)
+            {
+                while (!gameGrid.GameEnded && gameGrid.IsPlaying)
+                {
+                    await Task.Delay(CalculateDelay);
+                    gameGrid.Update(CalculateDelay);
+                    Draw();
+                }
+                await Task.Delay(100);
+            }
+        }               
+        private void Draw()
+        {
+            var matrix = gameGrid.Grid;
+            for (int r = 0; r < matrix.Length; r++)
+            {
+                for (int c = 0; c < matrix[0].Length; c++)
+                {
+                    int id = matrix[r][c];
+                    imageControls[r, c].Source = tileImages[id];
+                }
+            }
+            var figure = gameGrid.ActiveFigure;
+            foreach (var item in figure.container)
+            {
+                imageControls[item.x, item.y].Source = tileImages[item.Type];
+            }
+
+            lines.Text = $"Burned lines: {gameGrid.BurnedLines}";
+            level.Text = $"Level: {gameGrid.GameSpeed}";
+            score.Text = gameGrid.Scores.ToString();
+
+            DrawNextFigureCanvas();
+        }
+
+        private void GridKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Down:
+                case Key.S:
+                    gameGrid.TryMoveFigureDownFast();
+                    break;
+                case Key.Left:
+                case Key.A:
+                    gameGrid.TryMoveFigureLeft();
+                    break;
+                case Key.Right:
+                case Key.D:
+                    gameGrid.TryMoveFigureRight();
+                    break;
+                case Key.W:
+                case Key.Up:
+                    gameGrid.TryRotateFigure();
+                    break;
+            }
+        }
+        private void MainButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (gameGrid.GameEnded)
+            {
+                gameGrid.StartNewGame();
+            }
+            else
+            {
+                gameGrid.IsPlaying = !gameGrid.IsPlaying;
+            }
+            img.Source = button_images[!gameGrid.GameEnded && gameGrid.IsPlaying ? 0 : 1];
+        }
+    }
+}
